f79617048db45e4bb7946335af815433065411bb
GauravShukla-bit <79632204+GauravShukla-bit@users.noreply.github.com>
2024-08-22 14:14:12 +0000

    02_variables

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 12345;
+let accountName = "SBI";
+var accountCode = "1234";
+acoountAddress = "Bangaluru"
+
+// accountID = "98765"; //Constant can never be chnaged.
+accountName = "HDFC";
+accountCode = "6789";
+accountAddress = "Karnatka";
+// Var keyword can never be used for declaring variable because of block scope issue.
+
+console.table([accountID,accountName,accountCode,accountAddress]);
